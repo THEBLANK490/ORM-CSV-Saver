da23409ef029ce44dee90400e6d69de6c9c7bc47
THEBLANK490 <sanishmaharjan490@gmail.com>
2023-12-13 19:55:40 +0545

    update delete

--- assignment1_csv.py
+++ assignment1_csv.py
@@ -1,45 +1,144 @@
 import csv
 
 class CSV_Saver:
-    # that offers a comprehensive set of methods for managing CSV data.
-    # reading
-    # writing
-    # updating 
-    # deleting in rows
-    # static method to create a new CSV file with specified column headings 
+    id = 0
+    #constructor for the CSV_Saver class 
     def __init__(self,file_name):
         self.file_name = file_name
+        self.data =[]
 
+    # a method to read the data from csv file
     def reading(self):
-        with open(self.file_name,'r') as file :
-            print(file.readlines())
+        with open(self.file_name,'r',newline='') as file :
+            self.data = list(csv.reader(file))
+            return self.data
 
-    def updating(self):
-        with open(self.file_name,'a') as file :
-            pass
+    #a method for creating new data in csv file
+    def create_data(self,new_data):
+        with open(self.file_name,'a',newline='') as file :
+            csv_write = csv.writer(file)
+            csv_write.writerow(new_data)
+            print("Data Entered Successfully!!!")
+            print('')
 
-    def deleting_data_rows(self):
-        pass
+    # a method for updating the rows in the csv file 
+    def updating(self, id, updated_data):
+        found = False
 
+        #read the CSV file 
+        rows = self.reading()
+
+        #iterate through the rows and select the specified ID to update and set the flag(found) to True
+        for i, row in enumerate(rows):
+            if row[0] == str(id):
+                rows[i] = updated_data
+                found = True
+                break
+        
+        #if found then update the value else say row doesnot exists
+        if found:
+            with open(self.file_name, 'w', newline='') as file:
+                csv_writer = csv.writer(file) #csv writer object
+                csv_writer.writerows(rows) # write the updated rows back to csv_file
+            print(f"Row with ID {id} updated successfully.")
+        else:
+            print(f"Row with ID {id} does not exist.")
+
+    #a method for deleting the rows in the csv file
+    def deleting_data_rows(self, id_to_delete):
+        found = False
+
+        # Read the CSV file
+        rows = self.reading()
+
+        # Check if the row with the specified ID exists
+        for i, row in enumerate(rows):
+            if row[0] == str(id_to_delete):
+                found = True
+                del rows[i]  # Remove the row if found
+                break
+
+        if found:
+            with open(self.file_name, 'w', newline='') as file:
+                csv_writer = csv.writer(file)
+                csv_writer.writerows(rows)
+            print(f"Row with ID {id_to_delete} deleted successfully.")
+        else:
+            print(f"Row with ID {id_to_delete} does not exist.")
+
+    #a static method to crete the heading row in the csv file
     @staticmethod
     def create_CSV(file_name,heading):
-        with open(file_name,"w") as file:
+        with open(file_name,"w", newline='') as file:
             csv_write = csv.writer(file)
-            csv_write.writerows(heading)
+            csv_write.writerow(heading)
+            
+
+    #a method to read the input from the user and returns them in a list
+    def get_new_data(cls):
+        cls.id += 1
+        name = input("Enter your name: ")
+        age = input("Enter your age: ")
+        city = input("Enter the city name: ")
+        return [CSV_Saver.id,name,age,city]
 
 class CSV_Operation(CSV_Saver):
     # This child class will provide a tailored interface for specific data manipulation tasks.
-    def __init__(self)-> None:
-        super().__init__()
-        # initialize the CSV file nave and column headings
-        pass
-    # methods to Create , Update data rows, utilizing functionalities from parent class   
-    pass
+    def __init__(self,file_name):
+        super().__init__(file_name)
 
+    def create(self):
+        new_data = self.get_new_data()
+        self.create_data(new_data)
+
+    def update(self,id,updated_data):
+        self.updating(id,updated_data)
+    
 file_name = "test.csv"
-headings = ["Name", "Age", "City"]
 
-CSV_Saver.create_CSV(file_name,headings)
+#create an object of the parent class CSV_Saver
 csv_saver = CSV_Saver(file_name)
-csv_saver.reading()
+
+while True:
+    print("""Select from the below table:
+        1. Press 1 to create the table with the heading.
+        2. Press 2 to write the data into the table.
+        3. Press 3 to read the data from the table. 
+        4. Press 4 to update the data.
+        5. Press 5 to delete the data. \n""")
+    
+    choice = input("Enter YOur Choice: ")
+    if choice == '1':
+        heading = ["id","Name", "Age", "City"]
+        # calling the static method to create the heading 
+        CSV_Saver.create_CSV(file_name,heading)
+
+    elif choice == '2':
+        #ask the user for input by accessing the method in the csv_saver class
+        new_data =  csv_saver.get_new_data()
+        #to create the data accessing the create_data method 
+        csv_saver.create_data(new_data)
+
+    elif choice == '3':
+        #access the method reading in the CSV_saver class
+        result = csv_saver.reading()
+        print(result)
+
+    elif choice == '4':
+        index_to_update = int(input("Enter the Id to update: "))
+        updated_data = csv_saver.get_new_data()
+        csv_saver.updating(index_to_update, updated_data)
+    
+    elif choice == '5':
+        id_to_delete = input("Enter the ID to delete: ")
+        csv_saver.deleting_data_rows(id_to_delete)
+
+    elif choice == '6':
+        csv_operation = CSV_Operation(file_name)
+        csv_operation.create()
+
+    else:
+        print("Invalid choice. Please enter a valid option.")
+
+
 
